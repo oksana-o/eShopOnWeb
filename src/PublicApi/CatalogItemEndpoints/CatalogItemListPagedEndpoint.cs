0325394cd3ddc5030a131beca643be5f5154dfbe
oksana-o <150720316+oksana-o@users.noreply.github.com>
2024-01-04 10:21:58 +0200

    Update CatalogItemListPagedEndpoint.cs

--- src/PublicApi/CatalogItemEndpoints/CatalogItemListPagedEndpoint.cs
+++ src/PublicApi/CatalogItemEndpoints/CatalogItemListPagedEndpoint.cs
@@ -47,24 +47,24 @@ public class CatalogItemListPagedEndpoint : IEndpoint<IResult, ListPagedCatalogI
     {
         await Task.Delay(1000);
         var response = new ListPagedCatalogItemResponse(request.CorrelationId());
-
+    
         var filterSpec = new CatalogFilterSpecification(request.CatalogBrandId, request.CatalogTypeId);
         int totalItems = await itemRepository.CountAsync(filterSpec);
-
+    
         var pagedSpec = new CatalogFilterPaginatedSpecification(
             skip: request.PageIndex * request.PageSize,
             take: request.PageSize,
             brandId: request.CatalogBrandId,
             typeId: request.CatalogTypeId);
-
+    
         var items = await itemRepository.ListAsync(pagedSpec);
-
+    
         response.CatalogItems.AddRange(items.Select(_mapper.Map<CatalogItemDto>));
         foreach (CatalogItemDto item in response.CatalogItems)
         {
             item.PictureUri = _uriComposer.ComposePicUri(item.PictureUri);
         }
-
+    
         if (request.PageSize > 0)
         {
             response.PageCount = int.Parse(Math.Ceiling((decimal)totalItems / request.PageSize).ToString());
@@ -73,7 +73,7 @@ public class CatalogItemListPagedEndpoint : IEndpoint<IResult, ListPagedCatalogI
         {
             response.PageCount = totalItems > 0 ? 1 : 0;
         }
-        _logger.LogInformation("Returned X items");
+        _logger.LogInformation($"Returned {items.Count} items");    //<-- Notice the change here
         return Results.Ok(response);
     }
 }
